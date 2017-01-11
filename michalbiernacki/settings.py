3fa875aaa850128dc5f14358f402298dd0f2b821
biernacki <khortur@gmail.com>
2017-01-11 19:24:07 +0100

    Add basic logic of articles and store apps

--- michalbiernacki/settings.py
+++ michalbiernacki/settings.py
@@ -1,3 +1,5 @@
+# manage.py migrate --fake <app-name> zero
+
 """
 Django settings for michalbiernacki project.
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
